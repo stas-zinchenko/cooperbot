3a593e2127cbe5bc5308688349204693c2bb859d
Stanislav Zinchenko <83165298+redman-dev29@users.noreply.github.com>
2023-05-01 11:53:55 +0000

    Add files to the project new file:   LICENSE new file:   cmd/cooperbot.go new file:   cmd/root.go new file:   cmd/version.go new file:   cooperbot new file:   go.mod new file:   go.sum new file:   main.go

---
+++ cmd/cooperbot.go
@@ -0,0 +1,70 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken bot
+	TeleToken = "6247607846:AAGF5NIxZdv7jDMRR4eDh0CWtmvP9N5IJD4"
+)
+
+// cooperbotCmd represents the cooperbot command
+var cooperbotCmd = &cobra.Command{
+	Use:     "cooperbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("cooperbot %s started", appVersion)
+		cooperbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+		cooperbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello. I'm Cooperbot %s!", appVersion))
+
+			}
+			return err
+		})
+		cooperbot.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cooperbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cooperbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cooperbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/redman-dev29/cooperbot/cmd"
+
+func main() {
+	cmd.Execute()
+}
